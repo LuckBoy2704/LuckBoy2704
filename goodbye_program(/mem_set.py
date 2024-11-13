f7351f6b348398887cb23afb22e75a22e7cbb9f3
LuckBoy2704 <golodiukvlad2008@gmail.com>
2024-11-13 09:01:09 +0200

    Add files via upload

---
+++ goodbye_program(/mem_set.py
@@ -0,0 +1,12 @@
+import telebot
+from timing import show_time
+
+
+@show_time
+def men_sett(bot,message,text,buttons):
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    for item in buttons:
+        button = telebot.types.KeyboardButton(item)
+        keyboard.add(button)
+    for t in text:
+        bot.send_message(message.chat.id, t, reply_markup=keyboard)
